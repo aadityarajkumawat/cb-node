23d912398107559833142c9535d31d291998d21b
Aditya Raj Kumawat <arkumawat78@gmail.com>
2021-02-24 01:43:13 +0530

    created src folder in copy folder

--- src/index.js
+++ src/index.js
@@ -4,19 +4,30 @@ const fsPromises = require("fs").promises;
 const fs = require("fs");
 const { Command } = require("commander");
 const program = new Command();
-
-// program.version('0.1.0').arguments('<folder>')
+const newFolder = "copyrepo";
+// cool
 
 (async () => {
+  fs.mkdir(`../${newFolder}`, (e) => {
+    if (e) throw new Error(e.message);
+  });
   const files = await fsPromises.readdir("../repo");
   for (let i = 0; i < 5; i++) {
     if (files[i] !== "src") {
       console.log(files[i]);
-      fs.createWriteStream(`../copyrepo/${files[i]}`);
+      fs.createWriteStream(`../${newFolder}/${files[i]}`);
       await fsPromises.copyFile(
         `../repo/${files[i]}`,
-        `../copyrepo/${files[i]}`
+        `../${newFolder}/${files[i]}`
       );
+    }
+    // This is a directory
+    else if (files[i] == "src") {
+      fs.mkdir(`../${newFolder}/src`, (e) => {
+        if (e) throw new Error(e.message);
+      });
+      const srcFiles = await fsPromises.readdir("../repo/src");
+      console.log(srcFiles);
     }
   }
 })().catch(console.error);
