21f2b161af82cc85ecd5d89f32e25f87133cbab2
Aditya Raj Kumawat <arkumawat78@gmail.com>
2021-02-24 01:17:01 +0530

    reading file names

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 #!/usr/bin/env node
 
-const fsPromises = require('fs').promises;
-const fs = require('fs');
+const fsPromises = require("fs").promises;
+const fs = require("fs");
+const { Command } = require("commander");
+const program = new Command();
 
-fs.createWriteStream('')
+// program.version('0.1.0').arguments('<folder>')
+
+(async () => {
+  const files = await fsPromises.readdir("../repo");
+  console.log(files);
+})().catch(console.error);
