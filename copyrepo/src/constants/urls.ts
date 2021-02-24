c90ff41262472274a21a7eece35f3dbca74ff065
Aditya Raj Kumawat <arkumawat78@gmail.com>
2021-02-24 13:20:45 +0530

    added command line to get folder name

--- copyrepo/src/constants/urls.ts
+++
@@ -1,3 +0,0 @@
-export const DB_URL = "postgres://postgres:postgres@localhost:5432/keepdb";
-export const FRONTEND_URL = "http://localhost:3000";
-export const __prod__ = process.env.NODE_ENV === "production";
--- src/index.js
+++ src/index.js
@@ -4,32 +4,42 @@ const fsPromises = require("fs").promises;
 const fs = require("fs");
 const { Command } = require("commander");
 const program = new Command();
-const newFolder = "copyrepo";
 const fsExtra = require("fs-extra");
 // cool
 
-(async () => {
-  fs.mkdir(`../${newFolder}`, (e) => {
-    if (e) throw new Error(e.message);
-  });
-  const files = await fsPromises.readdir("../repo");
-  for (let i = 0; i < 5; i++) {
-    if (files[i] !== "src") {
-      console.log(files[i]);
-      fs.createWriteStream(`../${newFolder}/${files[i]}`);
-      await fsPromises.copyFile(
-        `../repo/${files[i]}`,
-        `../${newFolder}/${files[i]}`
-      );
-    }
-    // This is a directory
-    else if (files[i] == "src") {
-      fs.mkdir(`../${newFolder}/src`, (e) => {
+program
+  .version("0.1.0")
+  .arguments("<folder>")
+  .description("Take folder name", {
+    newFolder: "Enter name of folder: ",
+  })
+  .action((newFolder) => {
+    (async () => {
+      fs.mkdir(`../${newFolder}`, (e) => {
         if (e) throw new Error(e.message);
       });
-      // const srcFiles = await fsPromises.readdir("../repo/src");
-      // console.log(srcFiles);
-      fsExtra.copySync("../repo/src", "../copyrepo/src", { overwrite: true });
-    }
-  }
-})().catch(console.error);
+      const files = await fsPromises.readdir("../repo");
+      for (let i = 0; i < 5; i++) {
+        if (files[i] !== "src") {
+          fs.createWriteStream(`../${newFolder}/${files[i]}`);
+          await fsPromises.copyFile(
+            `../repo/${files[i]}`,
+            `../${newFolder}/${files[i]}`
+          );
+        }
+        // This is a directory
+        else if (files[i] == "src") {
+          fs.mkdir(`../${newFolder}/src`, (e) => {
+            if (e) throw new Error(e.message);
+          });
+          // const srcFiles = await fsPromises.readdir("../repo/src");
+          // console.log(srcFiles);
+          fsExtra.copySync("../repo/src", `../${newFolder}/src`, {
+            overwrite: true,
+          });
+        }
+      }
+    })().catch(console.error);
+  });
+
+program.parse();
